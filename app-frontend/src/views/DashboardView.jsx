1d6595a981e616406dc35f7c7e6dd54b2bc98cc7
EdgarGmz <edgar_gomez90@outlook.com>
2025-05-17 23:48:02 -0600

    Se crea estructura base del Login, Register y DashBoard

---
+++ app-frontend/src/views/DashboardView.jsx
@@ -0,0 +1,11 @@
+import Dashboard from '../components/Dashboard';
+
+const DashboardView = () => {
+    return (
+        <div>
+            <Dashboard/>
+        </div>
+    );
+}
+
+export default DashboardView;
---
+++ app-frontend/src/views/LoginView.jsx
@@ -0,0 +1,9 @@
+import Login from '../components/Login'
+
+export default function LoginView() {
+    return (
+        <div className="login-container">
+            <Login/>
+        </div>
+    )
+}
