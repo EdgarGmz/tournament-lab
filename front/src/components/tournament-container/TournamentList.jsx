c927ed30dd0dbade264182e160d93ca3e228e6ab
EdgarGmz <edgar_gomez90@outlook.com>
2025-06-08 22:34:03 -0600

    Se crean constantes de torneos

--- front/src/components/tournament-container/TournamentList.jsx
+++ front/src/components/tournament-container/TournamentList.jsx
@@ -1,38 +1,8 @@
+import { tournaments } from "../../constants";
 import TournamentCard from "./TournamentCard";
 
-
 const TournamentList = () => {
-    const tournaments = [{
-        name: "Torneo de Ajedrez",
-        status: "En curso",
-        participants: 16,
-        description: "Un emocionante torneo de ajedrez con los mejores jugadores."
-    }, {
-        name: "Campeonato de Fútbol",
-        status: "Próximo",
-        participants: 32,
-        description: "El campeonato de fútbol más esperado del año."
-    }, {
-        name: "Liga de Videojuegos",
-        status: "Finalizado",
-        participants: 64,
-        description: "Competencia intensa en los videojuegos más populares."
-    }, {
-        name: "Torneo de Tenis",
-        status: "En curso",
-        participants: 8,
-        description: "Un torneo de tenis con los mejores jugadores locales."
-    }, {
-        name: "Maratón Anual",
-        status: "Próximo",
-        participants: 1000,
-        description: "La maratón más grande de la ciudad, abierta a todos."
-    }, {
-        name: "Competencia de Programación",
-        status: "Finalizado",
-        participants: 50,
-        description: "Desafía tus habilidades en programación con este torneo."
-    }]
+    
     
     return (
         <div className="container mt-5">
---
+++ front/src/constants.js
@@ -0,0 +1,31 @@
+export const tournaments = [{
+    name: "Torneo de Ajedrez",
+    status: "En curso",
+    participants: 16,
+    description: "Un emocionante torneo de ajedrez con los mejores jugadores."
+}, {
+    name: "Campeonato de Fútbol",
+    status: "Próximo",
+    participants: 32,
+    description: "El campeonato de fútbol más esperado del año."
+}, {
+    name: "Liga de Videojuegos",
+    status: "Finalizado",
+    participants: 64,
+    description: "Competencia intensa en los videojuegos más populares."
+}, {
+    name: "Torneo de Tenis",
+    status: "En curso",
+    participants: 8,
+    description: "Un torneo de tenis con los mejores jugadores locales."
+}, {
+    name: "Maratón Anual",
+    status: "Próximo",
+    participants: 1000,
+    description: "La maratón más grande de la ciudad, abierta a todos."
+}, {
+    name: "Competencia de Programación",
+    status: "Finalizado",
+    participants: 50,
+    description: "Desafía tus habilidades en programación con este torneo."
+}]
