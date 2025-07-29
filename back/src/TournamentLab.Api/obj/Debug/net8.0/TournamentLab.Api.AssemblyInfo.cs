7dc0dff4d5c6983462ce37c047d60af39ba2fdee
EdgarGmz <edgar_gomez90@outlook.com>
2025-07-28 21:10:51 -0600

    Se mejora aspecto visual de Login y Register

--- back/src/TournamentLab.Api/obj/Debug/net8.0/TournamentLab.Api.AssemblyInfo.cs
+++ back/src/TournamentLab.Api/obj/Debug/net8.0/TournamentLab.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TournamentLab.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f7e23c2fc5be824eabf2fa166f64b70cbc0aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c2c74e0e3e07a739a359e2cb0733b068bf4d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("TournamentLab.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TournamentLab.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
